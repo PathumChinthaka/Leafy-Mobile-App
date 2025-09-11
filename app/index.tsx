07f39bb52b515de48c3b17f661447e003fdd83d4
Pathum Chinthaka <pathum.chinthaka@intellimorph.ai>
2025-09-11 10:27:38 +0530

    setup navigation with react navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,11 @@
 import "../global.css";
-import { Text, View } from "react-native";
+import { NavigationIndependentTree } from "@react-navigation/native";
+import AppNavigator from "@/navigation/AppNavigator";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-2xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
+    <NavigationIndependentTree>
+      <AppNavigator />
+    </NavigationIndependentTree>
   );
 }
